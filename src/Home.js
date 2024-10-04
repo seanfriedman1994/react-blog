8646f5aba58c5dbaef5ea960df9ea6531ff9785a
Sean Friedman <sfriedman@msmgroupinc.com>
2024-10-03 23:11:57 -0400

    useeffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -12,6 +12,10 @@ const Home = () => {
   const handleDelete = (id) => {
     setBlogs(blogs.filter(x => x.id != id));
   };
+
+  useEffect(() => {
+    console.log('use effect');
+  });
 
   return ( 
     <div className="home">
