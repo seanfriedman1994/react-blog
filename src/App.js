9d4718e5d435636dab0a0bdd969ed9e9a65b6d11
Sean Friedman <sfriedman@msmgroupinc.com>
2024-10-05 17:47:24 -0400

    BlogList, json data, routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="/create">
+              <Create></Create>
+            </Route>
+            
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,14 @@
-import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-  const [blogs, setBlogs] = useState([
-    {title: "My new website", body: "Lorem Ipsum", author: "Mario", id: 1},
-    {title: "Welcome Party", body: "Lorem Ipsum", author: "Luigi", id: 2},
-    {title: "Web Dev", body: "Lorem Ipsum", author: "Yoshi", id: 3}
-  ]);
-
-  const handleDelete = (id) => {
-    setBlogs(blogs.filter(x => x.id != id));
-  };
-
-  useEffect(() => {
-    console.log('use effect');
-  });
+  const {data, isPending, error} = useFetch("http://localhost:8000/blogs");
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter(x => x.author === "Mario")} title="Mario's Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && <BlogList blogs={data} title="All Blogs" /> }
     </div>
    );
 }
